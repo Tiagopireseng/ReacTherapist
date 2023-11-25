fc9ae57b9aac7c589c5257eb68b2f036d08416e7
Tiago Siqueira <tiagosiqueira@vendmob.com.br>
2023-11-25 09:33:27 -0300

    Initial Header component

---
+++ reactherapist/src/components/Header.tsx
@@ -0,0 +1,29 @@
+// src/components/Header.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className="logo">
+        <Link to="/">
+          <button className="btn">
+            <img src="logo.svg" alt="Therapist Logo" />
+          </button>
+        </Link>
+        {/* <h2>Lousir Psi</h2> */}
+      </div>
+      <nav className="header-navbar">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/client-portal">Client Portal</Link></li>
+          <li><Link to="/appointments">Appointments</Link></li>
+          <li><Link to="/support">Support</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
